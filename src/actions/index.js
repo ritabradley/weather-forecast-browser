c57eaedb1c661f48043a3bea9af5d2a9034ce277
Rita Bradley <contact@ritabradley.com>
2018-10-17 16:13:30 -0700

    connect search bar to weather api in order be make requests

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd3b7f3f0c1d9ceafdb7ab44651f7c1dd';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export default function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
